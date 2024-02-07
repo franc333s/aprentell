7013a00c6d4c09f1acded88084ed07c76d92d2cc
franc333s <marinafase@gmail.com>
2024-02-07 18:22:33 +0100

    React installed and set up

---
+++ aprentell/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      <h1>Aprentell</h1>
+      <p className="read-the-docs">
+        Aprendre a aprendre - estudia millor      
+      </p>
+    </>
+  )
+}
+
+export default App
